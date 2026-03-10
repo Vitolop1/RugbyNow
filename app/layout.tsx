b990b6ddda5807d45e1f883070849b7b4b4557f3
Vito Loprestti <lopresttivito@gmail.com>
2026-03-10 12:10:27 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-[#0E4F33] text-white antialiased`}
       >
         {children}
       </body>

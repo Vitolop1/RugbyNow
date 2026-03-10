638c6e133a7a0f032c650ba67a8eefe9e1a57be3
Vito Loprestti <lopresttivito@gmail.com>
2026-03-10 12:01:28 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4088690490762441"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

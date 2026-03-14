6031056dbec4e38b1fe13905379b07871796fca4
Vito Loprestti <lopresttivito@gmail.com>
2026-03-14 18:38:23 -0400

    Fix Cloudflare Builds OpenNext build flow

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,12 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig, type OpenNextConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
+const config: OpenNextConfig = {
+	...defineCloudflareConfig({
 	incrementalCache: r2IncrementalCache,
-});
+	}),
+	buildCommand: "npm run build:next",
+};
+
+export default config;
